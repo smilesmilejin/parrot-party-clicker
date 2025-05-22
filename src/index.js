c88e0a1319d21d546cafde8ca779f4fa5d953696
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-22 16:34:10 -0400

    completed extra

--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ const state = {
     clickCountLabel: null,
     clickCount: 0,
     parrot: null,
+    boxOne: null,
+    boxTwo: null,
+    boxTwoText: null,
+    boxThree: null, 
+    boxFour: null,
 };
 
 const handleAddButtonClicked = (event) => {
@@ -46,10 +51,75 @@ const handleResetButtonClicked = (event) => {
     state.clickCountLabel.textContent = state.clickCount;
     // state.parrot.style.width = '100px';
     state.parrot.classList.remove('enlarged');
+};
+
+// Handle Box One Hover
+const handleBoxOneHover = (event) => {
+    state.boxOne.style.backgroundColor = 'pink';
+};
+
+const handleBoxOneMouseLeave = (event) => {
+    state.boxOne.style.backgroundColor = '';
+};
+
+// Handle Box Two
+const handleBoxTwoSpacekeyDown = (event) => {
+    console.log(event.code);
+
+    if (event.code == 'Space') {
+        console.log('Space key pressed!');
+        // state.boxTwo.style.backgroundColor = 'black';
+        state.boxTwoText.textContent = '🍮';
+    }
+};
+
+const handleBoxTwoSpacekeyUp = (event) => {
+    // state.boxOne.style.backgroundColor = '';
+    if (event.code === 'Space') {
+        console.log('Space key released!');
+        // state.boxTwo.style.backgroundColor = '';
+        state.boxTwoText.textContent = 'press space'; // rest the text
+  }
+};
+
+// Handle Box Three
+const handleBoxThreeClicked = (event) => {
+  // this works, but the parrot is no longer in the middle
+//   if (parrot.style.display === 'none') {
+//     parrot.style.display = 'block';
+//   } else {
+//     parrot.style.display = 'none';
+//   }
+    // this also works! parrot is in the middle
+    // parrot: This is a reference to an HTML element (in this case, your Party Parrot image).
+    // .classList: This is a special property of an element that lets you read or manipulate the list of CSS classes applied to it.
+    // .toggle('hidden'): This method checks if the class 'hidden' is currently applied to the element.
+    // If the class is present, .toggle() removes it.
+    // If the class is NOT present, .toggle() adds it.
+    parrot.classList.toggle('hidden');
+};
+
+
+// Handle Box Four
+const handleBoxFourDoubleClicked = (event) => {
+    alert('Box 4 was double clicked!');
 }
+
 const registerEvents = () => {
     state.addClickButton.addEventListener ('click', handleAddButtonClicked);
     state.resetClickButton.addEventListener('click', handleResetButtonClicked);
+    state.boxOne.addEventListener('mouseenter', handleBoxOneHover);
+    state.boxOne.addEventListener('mouseleave', handleBoxOneMouseLeave);
+
+    // this need to focue on boxTwo
+    // state.boxTwo.addEventListener('keydown', handleBoxSpacekeyDown);
+    // state.boxTwo.addEventListener('mouseleave', handleBoxSpacekeyUp);
+    document.addEventListener('keydown', handleBoxTwoSpacekeyDown);
+    document.addEventListener('keyup', handleBoxTwoSpacekeyUp);
+
+    state.boxThree.addEventListener('click', handleBoxThreeClicked);
+
+    state.boxFour.addEventListener('dblclick', handleBoxFourDoubleClicked);
 };
 
 const onLoaded = () => {
@@ -62,6 +132,11 @@ const loadedControls = () => {
     state.clickCountLabel = document.getElementById('click-count-label');
     state.resetClickButton = document.getElementById('reset-click-button');
     state.parrot = document.getElementById('parrot');
+    state.boxOne = document.getElementById('box-one');
+    state.boxTwo = document.getElementById('box-two');
+    state.boxTwoText = document.getElementById('box-two-text');
+    state.boxThree = document.getElementById('box-three');
+    state.boxFour = document.getElementById('box-four');
 };
 
 
